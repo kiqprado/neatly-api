d3fa75ebf4b2b1df28dac89a453183fea03afa73
Kaique Prado <kiqprado@outlook.com>
2025-07-30 09:20:33 -0300

    feat: fixing some issues before deploying

--- src/services/seed-items.ts
+++ src/services/seed-items.ts
@@ -2,7 +2,21 @@ import { prisma } from '../lib/prisma'
 
 import { categories, items } from '../utils/market-data'
 
+if(process.env.SEED_ENABLED !== 'true') {
+  console.log('Seed already done.')
+  process.exit(0)
+}
+
+
 async function SeedDataBase() {
+  const categoryCount = await prisma.category.count()
+  const itemCount = await prisma.item.count()
+
+  if( categoryCount > 0 || itemCount > 0) {
+    console.log('DB Already seeded')
+    return
+  }
+
   await prisma.category.createMany({
     data: categories
   })
@@ -10,13 +24,14 @@ async function SeedDataBase() {
   await prisma.item.createMany({
     data: items
   })
+
+  console.log('Seeded Succesfully')
 }
 
-SeedDataBase().then(() => {
-  console.log('Seeded Succesfully')
-}).catch((e) => {
-  console.log(e)
-  process.exit(1)
-}).finally(() => {
-  prisma.$disconnect()
-})
+SeedDataBase()
+  .catch((e) => {
+    console.log('Error on seed', e)
+    process.exit(1)})
+  .finally(() => {
+    prisma.$disconnect()
+  })
